465dd08d7c562df079a61208bc407fb7ccb4c901
Akaidkhan <akaidkhan@gmail.com>
2020-01-12 23:58:00 +0600

    2nd Scrap

---
+++ Web Scraping With Pyhton/Intermidiate_Scrap.py
@@ -0,0 +1,19 @@
+import urllib.request
+from bs4 import BeautifulSoup
+address = "https://www.rottentomatoes.com/m/star_wars_the_rise_of_skywalker/reviews"
+
+page= urllib.request.urlopen(address)
+
+soup = BeautifulSoup(page,'html.parser')
+#print(soup.prettify())
+
+review = soup.find_all('div',attrs={'class':'the_review'})
+#print(review)
+
+for i in review:
+    print("review:")
+    print(i.get_text(),"\n")
+
+
+
+
